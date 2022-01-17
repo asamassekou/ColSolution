a7fd38d436a5f069ee837d3c324b57c6dc5cbe53
asamassekou <83638793+asamassekou@users.noreply.github.com>
2022-01-17 16:37:30 +0100

    Update mod_accueil.php

--- modules/mod_accueil/mod_accueil.php
+++ modules/mod_accueil/mod_accueil.php
@@ -1,1 +1,16 @@
+<?php
 
+require_once 'cont_accueil.php';
+
+class ModAccueil{
+
+    private $controleur;
+
+    public function __construct() { 
+        $controleur = new ContAccueil();
+    }
+
+}
+
+
+?>
