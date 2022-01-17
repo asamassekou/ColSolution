7d34cb893a51294ae6b717630b396054c3850594
asamassekou <83638793+asamassekou@users.noreply.github.com>
2022-01-17 16:33:58 +0100

    Update mod_message.php

--- modules/mod_message/mod_message.php
+++ modules/mod_message/mod_message.php
@@ -1,1 +1,43 @@
 
+<?php
+
+require_once 'cont_message.php';
+require_once 'modele_message.php';
+require_once 'mod_message.php';
+require_once 'vue_message.php';
+
+class ModMessage{
+
+    public $controleurrr;
+    public $modele;
+    public $mod;
+    public $vue;
+    public function __construct(){
+		$this->controleurrr = new ContMessage();
+        $this->modele = new ModeleMessage();
+        $this->vue = new VueMessage();
+        switch($this->controleurrr->action){
+            case "message": // Envoyer des messages 
+              $this->controleurrr->message();
+              $this->controleurrr->form_message();
+                break;
+            case "lecture" : // Page affichant un message au complet 
+                $this->controleurrr->form_lecture();
+                break;
+            case "reception" : // Page contenant tous les messages reçu 
+                $this->controleurrr->form_reception();
+                break;
+            case "envoie" : // Page contenant tous les messages reçu 
+                $this->controleurrr->form_envoie();
+                break;
+            case "supprimer" : // Supprimer un message 
+                $this->controleurrr->supprimer();
+                break;
+            default :
+             break;
+        }
+		
+	}
+}
+ 
+?>
