7e43147672ae131a956162f8a3fdd10a458e12da
asamassekou <83638793+asamassekou@users.noreply.github.com>
2022-01-17 16:36:32 +0100

    Update mod_compte.php

--- modules/mod_compte/mod_compte.php
+++ modules/mod_compte/mod_compte.php
@@ -1,1 +1,43 @@
+<?php
 
+require_once 'cont_compte.php';
+require_once 'modele_compte.php';
+require_once 'vue.php';
+
+class ModCompte{
+
+    public $controlerr;
+    public $modele;
+    public $mod;
+    public $vue;
+    public function __construct(){
+		$this->controlerr = new ContCompte();
+        $this->modele = new ModeleCompte();
+        $this->vue = new Vue();
+        switch($this->controlerr->action){
+            case "compte":
+                $this->controlerr->form_compte();
+                break;
+            case "admin":
+                  $this->controlerr->form_admin();
+                  break;
+            case "signaler":
+                   $this->controlerr->signaler();
+                   $this->controlerr->form_signaler();
+                   break;  
+            case "signalement":
+                    $this->controlerr->form_signalement();
+                    break;
+            case "bannir":
+                    $this->controlerr->bannir();
+                    break;
+            case "deconnexion" :
+                $this->controlerr->deconnexion();
+            default:
+                    break;
+        }
+		
+	}
+}
+ 
+?>
