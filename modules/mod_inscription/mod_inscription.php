77777bf68a2cbe34c0050826e4393cad868c6f1a
asamassekou <83638793+asamassekou@users.noreply.github.com>
2022-01-17 16:34:27 +0100

    Update mod_inscription.php

--- modules/mod_inscription/mod_inscription.php
+++ modules/mod_inscription/mod_inscription.php
@@ -1,1 +1,34 @@
 
+<?php
+
+require_once 'cont_inscription.php';
+require_once 'modele_inscription.php';
+require_once 'mod_inscription.php';
+require_once 'vue.php';
+
+class ModInscription{
+
+    public $controleurr;
+    public $modele;
+    public $mod;
+    public $vue;
+    public function __construct(){
+		$this->controleurr = new ContInscription();
+        $this->modele = new ModeleInscription();
+        $this->vue = new Vue();
+        switch($this->controleurr->action){
+            case "inscription":
+              $this->controleurr->inscription();
+              $this->controleurr->form_inscription();
+                break;
+              case "deconnexion" :
+                $this->controleurr->deconnexion();
+               // header('Location:index.php');
+
+
+        }
+		
+	}
+}
+ 
+?>
