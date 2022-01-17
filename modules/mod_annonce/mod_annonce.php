83b3b7d1913c1fcdbd093da56bcffb4203e30556
asamassekou <83638793+asamassekou@users.noreply.github.com>
2022-01-17 16:37:02 +0100

    Update mod_annonce.php

--- modules/mod_annonce/mod_annonce.php
+++ modules/mod_annonce/mod_annonce.php
@@ -1,1 +1,30 @@
+<?php
 
+require_once 'cont_annonce.php';
+require_once 'modele_annonce.php';
+require_once 'mod_annonce.php';
+require_once 'vue.php';
+
+class ModAnnonce{
+
+    public $controleur;
+    public $modele;
+    public $mod;
+    public $vue;
+    public function __construct(){
+		$this->controleur = new ContAnnonce();
+        $this->modele = new ModeleAnnonce();
+        $this->vue = new Vue();
+        switch($this->controleur->action){
+            case "depotAnnonce":
+              $this->modele->annonce();
+              $this->controleur->form_depotAnnonce();
+                break;
+            case "consultAnnonce":
+              $this->controleur->consulterAnnonce();
+              break;
+        }
+	}
+}
+ 
+?>
