d4ab44fd6b47d3114d5c6332fc952d6ff63318a8
asamassekou <83638793+asamassekou@users.noreply.github.com>
2022-01-17 16:35:35 +0100

    Update mod_connexion.php

--- modules/mod_connexion/mod_connexion.php
+++ modules/mod_connexion/mod_connexion.php
@@ -1,1 +1,33 @@
 
+<?php
+
+require_once 'cont_connexion.php';
+
+require_once 'modele_connexion.php';
+require_once 'mod_connexion.php';
+require_once 'vue.php';
+
+class ModConnexion{
+
+    public $controleurr;
+    public $modele;
+    public $mod;
+    public $vue;
+    
+    public function __construct(){
+		    $this->controleurr = new ContConnexion();
+        $this->modele = new ModeleConnexion();
+        $this->vue = new Vue();
+        switch($this->controleurr->action){
+            case "connexion":
+              $this->controleurr->connexion();
+              $this->controleurr->form_connexion();
+                break;
+              case "deconnexion" :
+                $this->controleurr->deconnexion();
+        }
+		
+	}
+}
+ 
+?>
