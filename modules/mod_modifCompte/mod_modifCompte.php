d90349a384122ac860cf2da4da715f5c0c9ced3c
asamassekou <83638793+asamassekou@users.noreply.github.com>
2022-01-17 16:33:22 +0100

    Update mod_modifCompte.php

--- modules/mod_modifCompte/mod_modifCompte.php
+++ modules/mod_modifCompte/mod_modifCompte.php
@@ -1,1 +1,35 @@
 
+<?php
+
+require_once 'cont_modifCompte.php';
+require_once 'modele_modifCompte.php';
+require_once 'vue.php';
+
+class ModModifCompte{
+
+    public $controlerr;
+    public $modele;
+    public $mod;
+    public $vue;
+    public function __construct(){
+		$this->controlerr = new ContModifCompte();
+        $this->modele = new ModeleModifCompte();
+        $this->vue = new Vue();
+        switch($this->controlerr->action){
+            case "compte":
+                $this->modele->modifCompte();
+                $this->controlerr->form_modifCompte();
+                break;
+                case "annonce":
+                 // $this->modele->modifAnnonce();
+                  $this->controlerr->form_modifAnnonce();
+                  break;
+              case "deconnexion" :
+                $this->controlerr->deconnexion();
+
+        }
+		
+	}
+}
+ 
+?>
